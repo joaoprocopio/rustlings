452d424dd6aa942691f000d3fe008ba797de8c30
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-01-02 22:44:53 -0300

    quiz

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,12 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let price_per_apple: i32 = if quantity > 40 { 1 } else { 2 };
+
+    quantity * price_per_apple
+}
 
 fn main() {
     // You can optionally experiment here.
