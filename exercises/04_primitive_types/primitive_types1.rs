bc27300e908b85d097123a1db01208762e142cf2
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-01-02 22:41:28 -0300

    primite tpyes

--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -1,5 +1,16 @@
 // Booleans (`bool`)
 
+struct ToTestando {
+    teste_1: i32,
+    teste_2: i32,
+}
+
+impl ToTestando {
+    fn new(teste_1: i32, teste_2: i32) -> Self {
+        Self { teste_1, teste_2 }
+    }
+}
+
 fn main() {
     let is_morning = true;
     if is_morning {
@@ -8,7 +19,7 @@ fn main() {
 
     // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
     // The value of the variable should be the negation (opposite) of `is_morning`.
-    // let …
+    let is_evening = !is_morning;
     if is_evening {
         println!("Good evening!");
     }
